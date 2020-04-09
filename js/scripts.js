13239bc172289b2265dcf2469759a439ae1bdea6
me <me@me.me.com>
2020-04-09 15:16:58 -0700

    create a name array

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
   $("#formOne").submit(function(e){
     e.preventDefault();
-    var celebrityArray = [["bradpitt","milakunis"]["leonardodicaprio","jessicaalba"],["mattdamon",]]
+    var celebrityArray = [["bradpitt","milakunis",0]["leonardodicaprio","jessicaalba",0],["mattdamon","natalieportman",0]];
+    var nameArray = ["dog","candy","saturday","vacation","console"];
+    nameArray.forEach(function(item){
+      celebrityArray.forEach
+        $("input[name=" + item + "]"+":checked").hasClass
+    })
+    
   })
 });
