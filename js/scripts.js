79ce6e5a8e2cfd2fc580e637623412be84e044a6
me <me@me.me.com>
2020-04-09 21:04:46 -0700

    refactor to use nested for loop rather than nested foreach

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,24 @@
 $(document).ready(function(){
   $("#formOne").submit(function(e){
     e.preventDefault();
+    
     var celebrityArray = [["bradpitt","milakunis",0]["leonardodicaprio","jessicaalba",0],["mattdamon","natalieportman",0]];
     var nameArray = ["dog","candy","saturday","vacation","console"];
-    nameArray.forEach(function(item){
-      celebrityArray.forEach(function(celebrity)){
+    for(var i = 0; i < nameArray.length; i++){
+      for(var j = 0; j < celebrityArray.length; j++){
         if(
-        $("input[name=" + item + "]"+":checked").hasClass(celebrity[0]) &&
-        $("input[name=" + item + "]"+":checked").hasClass(celebrity[1]))
-        {
-          celebrity[2]++;
-        }
+          $("input[name=" + nameArray[i] + "]"+":checked").hasClass(celebrityArray[j][0]) &&
+          $("input[name=" + item + "]"+":checked").hasClass(celebrityArray[j][1])
+          )
+          {
+            celebrityArray[j][2]++;
+          }   
       }
+    }
+       
         
-    })
+    });
+    console.log(celebrityArray)
     var mapToNumber = celebrityArray.map(item => item[2]);
     var highest = Math.max(...mapToNumber);
     var filteredCelebrities = celebrityArray.filter(function(celebrity){
@@ -24,5 +29,5 @@ $(document).ready(function(){
         $("div.celebritydiv."+ celebrity[0]).show();
         $("div.celebritydiv."+ celebrity[1]).show();
     });
-  })
+  });
 });
