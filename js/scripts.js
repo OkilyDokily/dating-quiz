5642b324f65d6df132c6b9ef15c40dba98876aa4
me <me@me.me.com>
2020-04-10 14:11:57 -0700

    factor out filterCelebrities into business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,28 @@
+//business logic
+var celebrityArray = [["bradpitt","milakunis",0],["leonardodicaprio","jessicaalba",0],["mattdamon","natalieportman",0]];
+var nameArray = ["dog","candy","saturday","vacation","console"];
+
+function getFilteredCelebrities(){
+    var mapToNumber = celebrityArray.map(item => item[2]);
+    var highest = Math.max(...mapToNumber);
+    var filteredCelebrities = celebrityArray.filter(function(celebrity){
+      return celebrity[2] === highest; 
+    });
+    return filteredCelebrities;
+}
+
+function resetValues(){
+  for (i=0; i < celebrityArray.length; i++){
+    celebrityArray[i][2] = 0;
+  }
+};
+
 $(document).ready(function(){
   $("#formOne").submit(function(e){
     e.preventDefault();
-    
-    var celebrityArray = [["bradpitt","milakunis",0],["leonardodicaprio","jessicaalba",0],["mattdamon","natalieportman",0]];
-    
-    var nameArray = ["dog","candy","saturday","vacation","console"];
+
+    hide();
+
     for(var i = 0; i < nameArray.length; i++){
       for(var j = 0; j < celebrityArray.length; j++){
         if(
@@ -16,17 +34,21 @@ $(document).ready(function(){
           }   
       }
     }
-       
-    var mapToNumber = celebrityArray.map(item => item[2]);
-    var highest = Math.max(...mapToNumber);
-    var filteredCelebrities = celebrityArray.filter(function(celebrity){
-      return celebrity[2] === highest; 
-    });
+      
+    var filteredCelebrities = getFilteredCelebrities();
   
-    
+    $("#bestmatch").show();
     filteredCelebrities.forEach(function(celebrity){
         $("div.celebrityimg."+ celebrity[0]).show();
         $("div.celebrityimg."+ celebrity[1]).show();
     });
+
+    function hide(){
+      resetValues();
+      celebrityArray.forEach(function(celebrity){
+        $("div.celebrityimg."+ celebrity[0]).hide();
+        $("div.celebrityimg."+ celebrity[1]).hide();
+    });
+    }
   });
 });
