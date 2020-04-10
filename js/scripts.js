4b2d6f7edb286dae2fcb390d6e2937be3cf87a29
me <me@me.me.com>
2020-04-09 21:17:01 -0700

    debug filter method

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,14 @@ $(document).ready(function(){
   $("#formOne").submit(function(e){
     e.preventDefault();
     
-    var celebrityArray = [["bradpitt","milakunis",0]["leonardodicaprio","jessicaalba",0],["mattdamon","natalieportman",0]];
+    var celebrityArray = [["bradpitt","milakunis",0],["leonardodicaprio","jessicaalba",0],["mattdamon","natalieportman",0]];
+    
     var nameArray = ["dog","candy","saturday","vacation","console"];
     for(var i = 0; i < nameArray.length; i++){
       for(var j = 0; j < celebrityArray.length; j++){
         if(
           $("input[name=" + nameArray[i] + "]"+":checked").hasClass(celebrityArray[j][0]) &&
-          $("input[name=" + item + "]"+":checked").hasClass(celebrityArray[j][1])
+          $("input[name=" + nameArray[i] + "]"+":checked").hasClass(celebrityArray[j][1])
           )
           {
             celebrityArray[j][2]++;
@@ -17,13 +18,14 @@ $(document).ready(function(){
     }
        
         
-    });
-    console.log(celebrityArray)
+    
+    
     var mapToNumber = celebrityArray.map(item => item[2]);
     var highest = Math.max(...mapToNumber);
     var filteredCelebrities = celebrityArray.filter(function(celebrity){
-      celebrity[2] === highest; 
+      return celebrity[2] === highest; 
     });
+    console.log(filteredCelebrities);
     
     filteredCelebrities.forEach(function(celebrity){
         $("div.celebritydiv."+ celebrity[0]).show();
